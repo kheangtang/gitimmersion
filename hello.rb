9bfad3d1256f3faddcc9c68b66bfbdc4c8d17c90
kheangtang <ktang1@uncc.edu>
2016-04-07 15:13:30 +0000

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -1,6 +1,7 @@
 puts "Hello, World"
 puts "Hello, #{ARGV.first}!" 
-# Default is "World"
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com)
 name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
